5e2a98c1571cea25153e1ac940aa20736452622d
kg2407 <iamkg2407@gmail.com>
2022-06-14 12:52:05 +0530

    Add asynchronous file reading

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,12 +1,18 @@
 const fs = require('fs');
 const path = require('path')
 
-const textIn = fs.readFileSync(path.join(__dirname,'./txt/input.txt'), 'utf-8')
+// Blocking, synchronous way
+// const textIn = fs.readFileSync(path.join(__dirname,'./txt/input.txt'), 'utf-8')
+// console.log(textIn)
+// const textOut = `This is what we know about avocado: ${textIn}. \n Created on ${Date.now()}`
+// fs.writeFileSync(path.join(__dirname, './txt/output.txt'), textOut)
+// console.log('File has been written!')
 
-console.log(textIn)
-
-const textOut = `This is what we know about avocado: ${textIn}. \n Created on ${Date.now()}`
-
-fs.writeFileSync(path.join(__dirname, './txt/output.txt'), textOut)
-
-console.log('File has been written!')
+// Non-blocking, asynchronous way
+fs.readFile(path.join(__dirname,'./txt/start.txt'), 'utf-8', (err, data)=>{
+  if (err){
+    throw err
+  }
+  console.log(data)
+})
+console.log('Will read the file asynchronously!')
